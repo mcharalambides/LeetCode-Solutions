05e3af237b1e114cd837913d908bf4f8fb68dc8c
Marios Charalambides <63081818+mcharalambides@users.noreply.github.com>
2022-11-16 23:29:06 +0200

    Time: 117 ms (47.58%), Space: 14 MB (31.53%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -8,17 +8,21 @@ class Solution:
         last_num = nums[0]
         counter = 1
         k = 0
+        dummy_num = max(nums)
         while i < len(nums):
             if nums[i] == last_num:
                 if counter >= 2:
-                    nums[i] = max(nums) + 1
+                    nums[i] = dummy_num
                     k += 1
                 counter += 1
             else:
                 last_num = nums[i]
                 counter = 1
             i = i + 1
-
-        nums.sort()
+        
+        
+        for i in range(0,k):
+            nums.remove(dummy_num)
+            nums.append(0)
         return len(nums) - k
         
