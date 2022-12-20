bdfc62a195ae1de15611e778fd47b1df082b880f
Marios Charalambides <63081818+mcharalambides@users.noreply.github.com>
2022-12-21 01:37:19 +0200

    Time: 1417 ms (61.97%), Space: 0B (100.00%) - LeetHub

--- 1158-market-analysis-i/1158-market-analysis-i.sql
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -1,1 +1,4 @@
-SELECT Users.user_id AS buyer_id, Users.join_date, SUM(IF(Orders.order_date >= '2019-01-01',1,0)) AS orders_in_2019 FROM Users LEFT JOIN Orders ON Users.user_id=Orders.buyer_id GROUP BY Users.user_id ORDER BY Users.user_id;
+SELECT Users.user_id AS buyer_id, Users.join_date, SUM(IF(Orders.order_date >= '2019-01-01',1,0)) AS orders_in_2019 FROM Users 
+LEFT JOIN Orders ON Users.user_id = Orders.buyer_id 
+GROUP BY Users.user_id 
+ORDER BY Users.user_id;
